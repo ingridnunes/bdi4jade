41edd540e9a6350f490e45fc2926bacf31a037df
João Faccin <jgui_faccin@outlook.com>
2015-09-20 10:52:04 -0300

    Example project updated.

--- bdi-jade-extensions/src/bdi4jade/extension/planselection/learningbased/example/Outcome01.java
+++ bdi-jade-extensions/src/bdi4jade/extension/planselection/learningbased/example/Outcome01.java
@@ -3,10 +3,33 @@ package bdi4jade.extension.planselection.learningbased.example;
 import bdi4jade.extension.planselection.learningbased.Outcome;
 
 public class Outcome01 extends Outcome {
+	
+	private static double min;
+	private static double max;
 
 	@Override
 	public double getMeasurement() {
 		return 1;
+	}
+	
+	@Override
+	public double getMin() {
+		return min;
+	}
+	
+	@Override
+	public void setMin(double min) {
+		Outcome01.min = min;
+	}
+	
+	@Override
+	public double getMax() {
+		return max;
+	}
+	
+	@Override
+	public void setMax(double max) {
+		Outcome01.max = max;
 	}
 
 }
--- bdi-jade-extensions/src/bdi4jade/extension/planselection/learningbased/example/Outcome02.java
+++ bdi-jade-extensions/src/bdi4jade/extension/planselection/learningbased/example/Outcome02.java
@@ -3,10 +3,32 @@ package bdi4jade.extension.planselection.learningbased.example;
 import bdi4jade.extension.planselection.learningbased.Outcome;
 
 public class Outcome02 extends Outcome {
+	
+	private static double min;
+	private static double max;
 
 	@Override
 	public double getMeasurement() {
 		return 0;
 	}
-
+	
+	@Override
+	public double getMin() {
+		return min;
+	}
+	
+	@Override
+	public void setMin(double min) {
+		Outcome02.min = min;
+	}
+	
+	@Override
+	public double getMax() {
+		return max;
+	}
+	
+	@Override
+	public void setMax(double max) {
+		Outcome02.max = max;
+	}
 }
